c6bfcaff57c5222f0a977fcbd522c58b83ebf8ec
AntoDono <anthony20151128@gmail.com>
2024-03-12 00:26:36 -0400

    added setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt install libopenblas-base
-sudo apt-get install libatlas-base-dev
-sudo apt-get install portaudio19-dev
+sudo apt install libopenblas-base -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install portaudio19-dev -y
