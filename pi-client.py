dab2d672922f314d6e0729e1423f042d9aa73f7a
AntoDono <anthony20151128@gmail.com>
2024-03-18 18:47:01 -0400

    added voice and max size

--- pi-client.py
+++ pi-client.py
@@ -7,10 +7,12 @@ from PIL import Image
 import base64
 from LM.sr import SpeechRecognition
 from aioconsole import ainput
+import espeak
 
 CAMERA_INDEX=0
 MIC_INDEX=0
 SOCKET_URL="ws://192.168.1.249:8000"
+MAX_SIZE_BYTES = 10 * 1024 * 1024
 GENERATION_END = "GEN_END"
 
 sr = SpeechRecognition(device_index=MIC_INDEX)
@@ -18,6 +20,11 @@ sr = SpeechRecognition(device_index=MIC_INDEX)
 print('\n'.join(sr.getDevices()))
 mic_index = int(input("Which mic device? "))
 sr.setDevice(device_index=mic_index)
+
+def speak_female(text):
+    espeak.set_voice("en+f3")
+    espeak.set_parameter(espeak.Parameter.Rate, 150)
+    espeak.synth(text)
 
 def getVision():
     camera = cv2.VideoCapture(index=CAMERA_INDEX)
@@ -56,16 +63,20 @@ async def user_handler(websocket):
             })
         )
 
+        message = ""
+
         async for res in websocket:
             response = json.loads(res)
             word = response.get("content")
             if (GENERATION_END == word):
+                speak_female(message)
                 break
+            message += word
             print(word, end="", flush=True)  
         print()
 
 async def connect_and_generate():
-    async with websockets.connect(SOCKET_URL) as websocket:
+    async with websockets.connect(SOCKET_URL, max_size=MAX_SIZE_BYTES) as websocket:
         ping_task = asyncio.create_task(ping_intervals(websocket, 10))
         handle_user = asyncio.create_task(user_handler(websocket))
 
