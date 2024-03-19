fc18d18e9095acb96893dfdd48ac1647ce52093c
AntoDono <anthony20151128@gmail.com>
2024-03-18 21:21:38 -0400

    fixing voice

--- pi-client.py
+++ pi-client.py
@@ -36,7 +36,7 @@ async def speak_female(text):
     speaker.say(text)
     movingMouth = asyncio.create_task(moveMouth())
     while speaker.playing():
-        asyncio.sleep(0.01)
+        asyncio.sleep(0.1)
     movingMouth.cancel()
     print("stopped yapping")
 
